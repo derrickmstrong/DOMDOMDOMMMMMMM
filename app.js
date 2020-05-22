292e70c43d92058c3243cc1a4e4218ebd706e402
Derrick Strong <derrickmstrong@gmail.com>
2020-05-22 15:55:08 -0500

    Added DOMContentLoaded event

--- app.js
+++ app.js
@@ -1,58 +1,61 @@
-let idNum = 1;
+document.addEventListener('DOMContentLoaded', function () {
+  let idNum = 1;
 
-const addSquare = document.createElement('button');
-addSquare.textContent = 'Add Square';
-document.body.appendChild(addSquare);
+  const addSquare = document.createElement('button');
+  addSquare.textContent = 'Add Square';
+  document.body.appendChild(addSquare);
 
-const divContainer = document.createElement('div');
-divContainer.classList.add('container');
-document.body.appendChild(divContainer);
+  const divContainer = document.createElement('div');
+  divContainer.classList.add('container');
+  document.body.appendChild(divContainer);
 
-addSquare.addEventListener('click', function () {
-  const div = document.createElement('div');
-  div.className = 'box';
-  div.setAttribute('id', idNum);
-  idNum++;
-  divContainer.appendChild(div);
+  addSquare.addEventListener('click', function () {
+    const div = document.createElement('div');
+    div.className = 'box';
+    div.setAttribute('id', idNum);
+    idNum++;
+    divContainer.appendChild(div);
 
-  div.addEventListener('click', randomColor);
-  div.addEventListener('mouseover', showId);
-  div.addEventListener('mouseout', hideId);
-  div.addEventListener('dblclick', removeDiv);
+    div.addEventListener('click', randomColor);
+    div.addEventListener('mouseover', showId);
+    div.addEventListener('mouseout', hideId);
+    div.addEventListener('dblclick', removeDiv);
 
-  function randomColor(e) {
-    const colors = [
-      'blue',
-      'red',
-      'brown',
-      'green',
-      'purple',
-      'orange',
-      'pink',
-    ];
-    const int = Math.floor(Math.random() * Math.floor(colors.length));
-    e.target.style.backgroundColor = colors[int];
-    e.target.style.color = 'white';
-  }
-  function showId(e) {
-    e.target.textContent = div.id;
-  }
-  function hideId(e) {
-    e.target.textContent = '';
-  }
-  function removeDiv(e) {
-    if (div.id % 2 === 0) {
-      if (div.nextElementSibling) {
-        e.target.nextElementSibling.remove();
+    // Function Definitions
+    function randomColor(e) {
+      const colors = [
+        'blue',
+        'red',
+        'brown',
+        'green',
+        'purple',
+        'orange',
+        'pink',
+      ];
+      const int = Math.floor(Math.random() * Math.floor(colors.length));
+      e.target.style.backgroundColor = colors[int];
+      e.target.style.color = 'white';
+    }
+    function showId(e) {
+      e.target.textContent = div.id;
+    }
+    function hideId(e) {
+      e.target.textContent = '';
+    }
+    function removeDiv(e) {
+      if (div.id % 2 === 0) {
+        if (div.nextElementSibling) {
+          e.target.nextElementSibling.remove();
+        } else {
+          alert('No Next Sibling!');
+        }
       } else {
-        alert('No Next Sibling!');
-      }
-    } else {
-      if (div.previousElementSibling) {
-        e.target.previousElementSibling.remove();
-      } else {
-        alert('No Previous Sibling!');
+        if (div.previousElementSibling) {
+          e.target.previousElementSibling.remove();
+        } else {
+          alert('No Previous Sibling!');
+        }
       }
     }
-  }
+  });
 });
